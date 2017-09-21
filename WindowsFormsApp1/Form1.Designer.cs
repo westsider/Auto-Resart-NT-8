81dad209bdd166cbceaef052869d7f9fccd7627f
Warren Hansen <whansen1@mac.com>
2017-09-20 22:50:34 -0700

    initial commit

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto Restart";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,48 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
+        private int i;
+        private Process process;
+
         public Form1()
         {
             InitializeComponent();
+
+            launchApplication();
+
+            
+
+            //MARK: - TODO Launch 64 bit
+
+            //MARK: - TODO Start time
+
         }
+
+        private async void launchApplication()
+        {
+            Console.WriteLine("Launching");
+
+            process = System.Diagnostics.Process.Start("C:\\Program Files (x86)\\NinjaTrader 8\\bin\\NinjaTrader");
+            
+            Console.WriteLine("Waiting to restart");
+
+            await PutTaskDelay();
+
+            Console.WriteLine("Quitting Application");
+
+            process.Kill();
+        }
+
+        async Task PutTaskDelay()
+        {
+       
+            for (i = 0; i < 15; i++)
+            {
+                Console.WriteLine(i);
+                await Task.Delay(1000);
+            }
+        }
+
+   
     }
 }
