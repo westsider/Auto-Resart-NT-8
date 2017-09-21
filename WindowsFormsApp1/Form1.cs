5712b4b1485eed55f10920762a1c64a4218c5f26
Warren Hansen <whansen1@mac.com>
2017-09-20 23:07:56 -0700

    task: Split Start func

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -22,8 +22,11 @@ namespace WindowsFormsApp1
 
             launchApplication();
 
-            //MARK: - TODO Start time
+            quitApplication();
 
+            //MARK: - TODO Split Start func
+            //MARK: - TODO Start time
+            //MARK: - TODO End time
         }
 
         private async void launchApplication()
@@ -34,17 +37,20 @@ namespace WindowsFormsApp1
             
             Console.WriteLine("Waiting to restart");
 
+        }
+
+        private async void quitApplication()
+        {
             await PutTaskDelay();
 
             Console.WriteLine("Quitting Application");
 
             process.Kill();
         }
-
-        async Task PutTaskDelay()
+            async Task PutTaskDelay()
         {
        
-            for (i = 0; i < 15; i++)
+            for (i = 0; i < 20; i++)
             {
                 Console.WriteLine(i);
                 await Task.Delay(1000);
